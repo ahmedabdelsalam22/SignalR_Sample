7afdf6205ce78808d05721c06be73f899c636103
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-11-08 23:50:18 +0200

    create connection in usersCount.js file and call hub methods and start connection

---
+++ SignalR_Sample/wwwroot/js/usersCount.js
@@ -0,0 +1,30 @@
+﻿// create connection
+
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+// connect to method that hub invokes aka recieves notifications from hub
+
+connectionUserCount.on("updateTotalViews", (value) => {
+
+    var newCountSpan = document.getElementById("totalViewsCounter");
+
+    newCountSpan.innerText = value;
+
+})
+
+// invoke hub methods aka send notification to hub
+
+function newWindowLoadedOnClient()
+{
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+// start connection
+
+function fulfilled()
+{
+    console.log("connection to user hub successfull");
+}
+function rejected() { }
+
+connectionUserCount.start().then(fulfilled,rejected);
